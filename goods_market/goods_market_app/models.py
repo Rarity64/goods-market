927794be6a6206f79542f78762f9aa9429397d12
EduardSuyargulov <edsuyargulov@gmail.com>
2026-02-28 13:04:19 +0500

    added blocks

--- goods_market/goods_market_app/models.py
+++ goods_market/goods_market_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from transliterate import translit
 
 # Good - продукт
 class Good(models.Model):
@@ -8,13 +9,28 @@ class Good(models.Model):
     # DateField()
     # DateTimeField()
     # FilePathField() - путь до файла
+
+    food_types = (
+        ('Фр', 'Фрукты'), 
+        ('Ов', 'Овощи'), 
+        ('Зел', 'Зелень'), 
+        ('Сл', 'Сладкое'), 
+        ('Мяс', 'Мясо')
+    )
+
+    def user_directory_path(instance, filename):
+        title = str(translit(value = instance.title, language_code = 'ru', reversed = True))
+        id = str(instance.id)
+        return f'goods/{id}_{title}/{filename}'
     
     title = models.CharField(max_length = 100) # Название продукта
     price = models.FloatField() # Цена продукта
     description = models.TextField() # Описание продукта
-    image = models.ImageField() # Картинка продукта
+    image = models.ImageField(default='none', upload_to=user_directory_path) # Картинка продукта
+    image2 = models.ImageField(default='none', upload_to=user_directory_path)
     quantity = models.IntegerField() # Количество продукта
     expiry_date = models.DateField() # Срок годности
+    food_type = models.CharField(max_length = 100, choices = food_types)
 
     def __str__(self):
         return f'{self.title}'
